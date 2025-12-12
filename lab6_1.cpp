1878a9c14efa35ed9176d8d8d42616c943340202
Yeen <yeenprc@gmail.com>
2025-12-12 20:38:54 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num = -1;
+    int odd = 0;
+    int even = 0;
+    while(num != 0){
+       cout << "Enter an integer: ";
+       cin >> num;
+       if(num == 0){
+           break;
+       }
+       if(num%2 == 0){
+        even ++;
+       }else{
+        odd ++;
+       }
+    }
+    cout << "#Even numbers = "<< even <<"\n";
+    cout << "#Odd numbers = "<< odd;
     return 0;
 }
